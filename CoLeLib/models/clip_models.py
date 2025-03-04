f15be6cccba8fa7468327a1060f23866921fe9ee
Muhammad Nawfal Meeran <muhdnawfal29@gmail.com>
2025-03-04 21:30:26 +0530

    initial commit

---
+++ CoLeLib/models/clip_models.py
@@ -0,0 +1,303 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from transformers import CLIPModel
+from transformers.modeling_attn_mask_utils import _create_4d_causal_attention_mask
+
+#############################################
+### CLIP Text Encoder Parameter-Efficient ###
+#############################################
+
+
+class CLIPTextModelForPromptTuning(nn.Module):
+    def __init__(
+        self, model: object, D_g: int, D_s: int, deep_replace_method: str = "replace"
+    ):
+        """
+        CLIP Text Encoder for PE
+        model: CLIP Text Encoder
+        D_g_l: number of layers to append general prompts
+        D_s: number of layers to append shared prompts
+        deep_replace_method: "replace", "accumulate", or "accumulate_same" prompts to every layer
+        """
+        super().__init__()
+        self.model = model
+        self.d_model = 512
+        self.D_g = D_g
+        self.D_s = D_s
+        self.deep_replace_method = deep_replace_method
+
+    def forward(
+        self,
+        text_tokens: torch.Tensor,
+        attn_mask: torch.Tensor,
+        g_prompt: torch.Tensor,
+        s_prompt: torch.Tensor,
+    ):
+        """
+        text_tokens: [batch_size, n_tokens]
+        attn_mask: [batch_size, n_tokens]
+        g_prompt: [text_tokens, n_deep, n_prompts, d_model]
+        s_prompt: [text_tokens, n_deep, n_prompts, d_model]
+        """
+        assert g_prompt.size(0) == s_prompt.size(0)
+        bs = g_prompt.size(0)
+
+        g_prompt = g_prompt.to(text_tokens.device)
+        s_prompt = s_prompt.to(text_tokens.device)
+
+        x = self.model.embeddings.token_embedding(text_tokens)
+        p = g_prompt[:, 0]
+        L_p = p.size(1)
+
+        x = torch.cat([x[:, 0:1, :], p, x[:, 1:, :]], dim=1)
+        x = x + self.model.embeddings.position_embedding(
+            torch.arange(x.size(1), device=attn_mask.device).unsqueeze(0)
+        )
+
+        total_num_layers = len(self.model.encoder.layers)
+
+        for i, l in enumerate(self.model.encoder.layers):
+            if i > 0:
+                if i < self.D_g:
+                    p = g_prompt[:, i]
+                elif i >= (total_num_layers - self.D_s):
+                    p = s_prompt[:, i - (total_num_layers - self.D_s)]
+
+                # if self.deep_replace_method == "accumulate":
+                #     previous_p_out = x[:, 1 : (L_p + 1), :]
+                #     p = torch.cat([previous_p_out, p], dim=1)
+                x = torch.cat([x[:, 0:1, :], p, x[:, (L_p + 1) :, :]], dim=1)
+                L_p = p.size(1)
+
+            attn_mask_ = torch.cat(
+                [torch.ones(bs, L_p, device=attn_mask.device), attn_mask], dim=-1
+            )
+
+            res = x
+            x = l.layer_norm1(x)
+
+            q = l.self_attn.q_proj(x) * 0.125
+            k = l.self_attn.k_proj(x)
+            v = l.self_attn.v_proj(x)
+
+            extended_attn_mask = (attn_mask_.unsqueeze(1).unsqueeze(1) == 0).float()
+            extended_attn_mask[extended_attn_mask == 1] = torch.finfo(x.dtype).min
+
+            q = q.view(x.size(0), x.size(1), 8, -1).transpose(1, 2)
+            k = k.view(x.size(0), x.size(1), 8, -1).transpose(1, 2)
+            v = v.view(x.size(0), x.size(1), 8, -1).transpose(1, 2)
+            w = q @ k.transpose(-1, -2)
+            
+            c_mask = (
+                _create_4d_causal_attention_mask((x.size(0), x.size(1)), torch.float, attn_mask.device)
+            )
+            w = w + c_mask + extended_attn_mask
+            w = w.softmax(dim=-1)
+            v = (w @ v).transpose(1, 2).contiguous().view(x.size(0), x.size(1), -1)
+            x = l.self_attn.out_proj(v)
+            x = res + x
+
+            res = x
+            x = l.layer_norm2(x)
+            x = l.mlp(x)
+
+            x = res + x
+
+        x = self.model.final_layer_norm(x)
+
+        index = text_tokens.argmax(dim=-1) + L_p
+        return x[torch.arange(x.size(0)), index]
+
+
+###############################################
+### CLIP Vision Encoder Parameter-Efficient ###
+###############################################
+
+
+class CLIPVisionModelForPromptTuning(nn.Module):
+    def __init__(
+        self, model: object, D_g: int, D_s: int, deep_replace_method: str = "replace"
+    ):
+        """
+        CLIP Vision Encoder for PE
+        model: CLIP Vision Encoder
+        deep_replace_method: "replace", "accumulate", or "accumulate_same" prompts to every layer
+        """
+        super().__init__()
+        self.model = model
+        self.d_model = 768
+        self.D_g = D_g
+        self.D_s = D_s
+        self.deep_replace_method = deep_replace_method
+
+    def forward(self, image: torch.Tensor, g_prompt: torch.Tensor, s_prompt: torch.Tensor):
+        """
+        image: [batch_size, 3, 224, 224]
+        g_prompt: [batch_size, n_deep, n_prompts, d_model]
+        """
+        x = self.model.embeddings(image)
+
+        p = g_prompt[:, 0]
+
+        x = torch.cat([x, p], dim=1)
+        x = self.model.pre_layrnorm(x)
+        L_p = p.size(1)
+
+        total_num_layers = len(self.model.encoder.layers)
+
+        for i, l in enumerate(self.model.encoder.layers):
+            if i > 0:
+                if i < self.D_g:
+                    p = g_prompt[:, i]
+                elif i >= (total_num_layers - self.D_s):
+                    p = s_prompt[:, i - (total_num_layers - self.D_s)]
+
+                # if self.deep_replace_method == "accumulate":
+                #     previous_p_out = x[:, -L_p:, :]
+                #     p = torch.cat([previous_p_out, p], dim=1)
+                x = torch.cat([x[:, :-L_p, :], p], dim=1)
+                L_p = p.size(1)
+
+
+            res = x
+            x = l.layer_norm1(x)
+
+            q = l.self_attn.q_proj(x) * 0.125
+            k = l.self_attn.k_proj(x)
+            v = l.self_attn.v_proj(x)
+
+            q = q.view(x.size(0), x.size(1), 12, -1).transpose(1, 2)
+            k = k.view(x.size(0), x.size(1), 12, -1).transpose(1, 2)
+            v = v.view(x.size(0), x.size(1), 12, -1).transpose(1, 2)
+            w = q @ k.transpose(-1, -2)
+            w = w.softmax(dim=-1)
+            v = (w @ v).transpose(1, 2).contiguous().view(x.size(0), x.size(1), -1)
+            x = l.self_attn.out_proj(v)
+            x = res + x
+
+            res = x
+            x = l.layer_norm2(x)
+            x = l.mlp(x)
+
+            x = res + x
+
+        return self.model.post_layernorm(x[:, 0, :])
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, dim_in=512, dim_out=768, num_heads=4):
+        super().__init__()
+        self.num_heads = num_heads
+        self.head_dim = dim_in // num_heads  # Split for multi-heads
+
+        # Linear layers for Q, K, V
+        self.q_proj = nn.Linear(dim_in, dim_in, bias=False)
+        self.k_proj = nn.Linear(dim_in, dim_in, bias=False)
+        self.v_proj = nn.Linear(dim_in, dim_in, bias=False)
+
+        # Projection to 768 after attention
+        self.proj = nn.Linear(dim_in, dim_out)
+
+    def forward(self, x):
+        # Compute Q, K, V
+        Q = self.q_proj(x)
+        K = self.k_proj(x)
+        V = self.v_proj(x)
+
+        # Efficient scaled dot-product attention (PyTorch 2.0+)
+        attn_output = F.scaled_dot_product_attention(Q, K, V)
+
+        # Project to 768
+        return self.proj(attn_output)
+    
+
+################################
+### CLIP Parameter-Efficient ###
+################################
+
+
+class CLIPParameterEfficient(nn.Module):
+    def __init__(
+        self,
+        L_g: int = 2,
+        L_s: int = 2,
+        D_g: int = 3,
+        D_s: int = 3,
+        text_deep_replace_method: str = "replace",
+        vision_deep_replace_method: str = "replace",
+    ):
+        """
+        CLIP Parameter-Efficient
+        L_g: number of g-prompts
+        deep_replace_method: "replace", "accumulate", or "accumulate_same" prompts to every layer
+        """
+        super().__init__()
+        self.clip_model = CLIPModel.from_pretrained("openai/clip-vit-base-patch16")
+
+        ### Text Encoder ###
+        self.text_model = CLIPTextModelForPromptTuning(
+            model=self.clip_model.text_model,
+            D_g=D_g,
+            D_s=D_s,
+            deep_replace_method=text_deep_replace_method,
+        )
+
+        ### Vision Encoder ###
+        self.vision_model = CLIPVisionModelForPromptTuning(
+            model=self.clip_model.vision_model,
+            D_g=D_g,
+            D_s=D_s,
+            deep_replace_method=vision_deep_replace_method,
+        )
+
+        self.image_proj = self.clip_model.visual_projection
+        self.text_proj = self.clip_model.text_projection
+        self.eval()
+        for p in self.parameters():
+            p.requires_grad = False
+
+        self.prompt_proj = TransformerBlock(self.text_model.d_model, self.vision_model.d_model)
+        # self.prompt_proj = nn.Linear(self.text_model.d_model, self.vision_model.d_model)
+        self.g_v_values = nn.Parameter(torch.zeros(D_g, L_g, self.vision_model.d_model))
+        self.g_l_values = nn.Parameter(torch.zeros(D_g, L_g, self.text_model.d_model))
+        self.s_values = nn.Parameter(torch.zeros(D_s, L_s, self.text_model.d_model))
+
+        nn.init.xavier_uniform_(self.s_values.data)
+
+        self.L_g = L_g
+        self.L_s = L_s
+        self.D_g = D_g
+        self.D_s = D_s
+
+    def forward(
+        self,
+        image: torch.Tensor,
+        text_tokens: torch.Tensor,
+        attn_mask: torch.Tensor,
+        device="cuda",
+    ):
+        """
+        image: [batch_size, 3, 224, 224]
+        text_tokens: [n_classes, max_length]
+        attn_mask: [n_classes, max_length]
+        """
+        batch_size = image.shape[0]
+
+        text_g_prompt = self.g_l_values.repeat(text_tokens.size(0), 1, 1, 1).to(device)
+        vision_g_prompt = self.g_v_values.repeat(batch_size, 1, 1, 1)
+        text_s_prompt = self.s_values.repeat(text_tokens.size(0), 1, 1, 1).to(device)
+        vision_s_prompt = self.prompt_proj(self.s_values).repeat(batch_size, 1, 1, 1)
+
+        text_out = self.text_model(text_tokens, attn_mask, text_g_prompt, text_s_prompt)
+        img_out = self.vision_model(image, vision_g_prompt, vision_s_prompt)
+
+        # Project to common dimensional space
+        text_proj = self.text_proj(text_out)
+        img_proj = self.image_proj(img_out)
+        # Normalize
+        text_embed = text_proj / text_proj.norm(dim=-1, keepdim=True)
+        img_embed = img_proj / img_proj.norm(dim=-1, keepdim=True)
+        sim = 100 * img_embed @ text_embed.T
+
+        return sim
---
+++ train.py
@@ -0,0 +1,84 @@
+import os
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+
+import warnings
+import argparse
+import gc
+import torch
+
+from transformers import CLIPProcessor
+
+from CoLeLib.datasets import CIFAR100FSCIL, CUB200FSCIL, MiniImageNetFSCIL
+from CoLeLib.training.strategies import CLIPPE
+
+warnings.filterwarnings("ignore")
+
+datasets = dict(
+    cub200 = dict(dataset = CUB200FSCIL, train_mb_size_base_class = 4, train_epochs_base_class = 6),
+    cifar100 = dict(dataset = CIFAR100FSCIL, train_mb_size_base_class = 32, train_epochs_base_class = 5),
+    miniimagenet = dict(dataset = MiniImageNetFSCIL, train_mb_size_base_class = 32, train_epochs_base_class = 5)
+)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--L_g", type=int, default=2, help="Number of general prompts to be used")
+parser.add_argument("--L_s", type=int, default=2, help="Number of shared prompts to be used")
+parser.add_argument("--D_g", type=int, default=4, help="Number of layers in which the general prompts will be processed")
+parser.add_argument("--D_s", type=int, default=8, help="Number of layers in which the shared prompts will be processed")
+# parser.add_argument("--text_deep_replace_method", type=str, default="replace", choices=["replace", "accumalate", "accumulate_same"], help="Method to replace the text prompts in the encoders. Options: replace, accumulate, accumulate_same")
+# parser.add_argument("--vision_deep_replace_method", type=str, default="accumulate", choices=["replace", "accumulate", "accumulate_same"], help="Method to replace the vision prompts in the encoders. Options: replace, accumulate, accumulate_same")
+parser.add_argument("--dataset_name", type=str, default="cifar100", choices=["cifar100", "cub200", "miniimagenet"], help="Name of the dataset to be used. Options: cifar100, cub200, miniimagenet")
+parser.add_argument("--n_runs", type=int, default=5, help="Number of runs to be executed", required=False)
+parser.add_argument("--seeds", type=int, nargs="+", help="Seeds to be used in the runs", required=False)
+# parser.add_argument("--ablation", type=str, choices=["no_accumulation", "no_regularization", "no_vision_prompts"])
+
+args = parser.parse_args()
+n_runs = args.n_runs
+dataset_name = args.dataset_name
+
+few_shot_examples = [5]
+
+assert n_runs > 0, "Number of runs must be greater than 0."
+# NOTE: Seeds used in the paper for 5 runs are: seeds = [42, 13, 50, 24, 69]
+seeds = args.seeds if args.seeds else [s for s in range(42, 42 + n_runs)]
+# ablation = args.ablation
+
+img_preprocess = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch16").feature_extractor
+
+if __name__ == "__main__":
+    for run in range(n_runs):
+        exp_name = f"model_{dataset_name}_L_g_{args.L_g}_L_s_{args.L_s}_D_g_{args.D_g}_D_s_{args.D_s}_run_{run}"
+        
+        Dataset = datasets[dataset_name]["dataset"]
+        train_mb_size_base_class = datasets[dataset_name]["train_mb_size_base_class"]
+        train_epochs_base_class = datasets[dataset_name]["train_epochs_base_class"]
+
+        strategy = CLIPPE(
+            device='cuda',
+            seed=seeds[run],
+            L_g=args.L_g,
+            L_s=args.L_s,
+            D_g=args.D_g,
+            D_s=args.D_s,
+            text_deep_replace_method="replace",
+            vision_deep_replace_method="replace",
+            regularization_method='balance',
+            train_mb_size_base_class=train_mb_size_base_class,
+            train_epochs_base_class=train_epochs_base_class,
+            lr=0.00325,
+            use_scheduler=True,
+            json_file_name=exp_name + ".json",
+            eval_mb_size=64
+        )
+        print(strategy.model)
+
+        experiences = Dataset(transforms=img_preprocess)
+
+        strategy.train(experiences)
+
+        torch.cuda.empty_cache()
+        gc.collect()
+
+        del strategy
+        del experiences
